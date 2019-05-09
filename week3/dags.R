ea69cc50a228248d7b34e9cf5fb2e26f12577eb1
nayyarv <nayyarv@gmail.com>
2019-05-09 18:39:22 +1000

    Some changes

--- week3/dags.R
+++ week3/dags.R
@@ -12,10 +12,10 @@ dag_6.2 <- dagitty( "dag {
 }")
 
 
-plot(dag_6.2 %>% graphLayout())
-adjustmentSets( dag_6.2 , exposure="S" , outcome="D" )
+ggdag(dag_6.2)
+ggdag_paths(dag_6.2, from="W", to="D")
+ggdag_adjustment_set( dag_6.2 , exposure="W" , outcome="D" )
 impliedConditionalIndependencies(dag_6.2)
-ggdag_paths(dag_6.2, from="S", to="D")
 
 
 
@@ -24,21 +24,22 @@ dagc = dagitty("dag{
   G -> C
   P <- U -> C
 }")
-plot(dagc %>% graphLayout())
-adjustmentSets( dagc, exposure="G" , outcome="C" )
+# 
+ggdag(dagc)
+ggdag_paths(dagc, "P" , "C" )
 
 impliedConditionalIndependencies(dagc)
-ggdag_paths(dagc, from="G", to="C")
+ggdag_paths(dagc, from="P", to="C")
 
 
 dag_6.1 <- dagitty( "dag {
-    X -> Y <- C
+   Y <- C
     X <- U -> B
     U <- A -> C
-    U -> B <- C
+    U -> B -> C
 }")
-
-adjustmentSets( dag_6.1 , 'B', "Y")
+ggdag(dag_6.1)
+adjustmentSets( dag_6.1 , 'X', "Y")
 ggdag_paths(dag_6.1, "X", "Y")
 ggdag_adjustment_set(dag_6.1, "X", "Y")
                  
